6bafc4ac94dd839a50fa95da0a9087f1f1da37f2
Ji Sun <ji@linux-4zu9.site>
2015-09-30 18:09:41 -0400

    problem 1 and 2

---
+++ p0002/solution.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int main(void) {
+    int UPPER_BOUND = 4000000;
+    int fib_1 = 2, fib_2 = 3, fib_3 = 5;
+    int result = 0;
+    while (fib_1 < UPPER_BOUND) {
+        result += fib_1;
+        fib_1 = fib_2 + fib_3;
+        fib_2 = fib_3 + fib_1;
+        fib_3 = fib_1 + fib_2;
+    }
+    printf("%d\n", result);
+    return 0;
+}
